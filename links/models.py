3617a6eaa7b895d45ba3c2b9c729a816613c2d87
emin <samburer200@gmail.com>
2021-04-23 17:23:49 +0300

    add modellink

--- links/models.py
+++ links/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    full_link = models.CharField()
+    short_link = models.CharField()
+    redirects = models.IntegerField()

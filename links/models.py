8f48c1533d286510e036b8a8907c16c75c44e650
emin <samburer200@gmail.com>
2021-04-27 18:40:21 +0300

    fix bugs with model

--- links/models.py
+++ links/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Link(models.Model):
-    full_link = models.CharField()
-    short_link = models.CharField()
-    redirects = models.IntegerField()
+    full_link = models.CharField(max_length=200)
+    short_link = models.CharField(max_length=50, unique=True)
+    redirects = models.IntegerField(default=0)
 
     def __str__(self):
         return self.short_link

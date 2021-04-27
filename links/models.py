504c58fe70b9f0b1b611a08091bede6710f369dd
emin <samburer200@gmail.com>
2021-04-27 18:26:02 +0300

    added string method for model

--- links/models.py
+++ links/models.py
@@ -5,3 +5,6 @@ class Link(models.Model):
     full_link = models.CharField()
     short_link = models.CharField()
     redirects = models.IntegerField()
+
+    def __str__(self):
+        return self.short_link

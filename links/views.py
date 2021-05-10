5fcfa21ad104d079d6c6479c83b05f0cf14f93b5
emin <samburer200@gmail.com>
2021-05-10 21:55:33 +0300

    add list views and templates

--- links/views.py
+++ links/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from links.models import Link
+
+
+class LinkListView(ListView):
+    template_name = ''
+    context_object_name = 'links_list'
+
+    def get_queryset(self):
+        return Link.objects.filter().order_by('-redirects')
+
+
+def index(request):
+    return render(request, 'base.html')

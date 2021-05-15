96ead13d3684361bc6dc4728bd82567d575d3697
emin <samburer200@gmail.com>
2021-05-15 22:01:01 +0300

    add urls for app

--- links/views.py
+++ links/views.py
@@ -1,16 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
 from links.models import Link
 
 
 class LinkListView(ListView):
-    template_name = ''
+    template_name = 'index.html'
     context_object_name = 'links_list'
 
     def get_queryset(self):
         return Link.objects.filter().order_by('-redirects')
-
-
-def index(request):
-    return render(request, 'base.html')

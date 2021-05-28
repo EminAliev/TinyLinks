0bf4012877f405e110a10a3f4adea363b43913bd
emin <samburer200@gmail.com>
2021-05-28 13:35:15 +0300

    add tests

--- links/tests.py
+++ links/tests.py
@@ -1,3 +1,73 @@
+from django.shortcuts import get_object_or_404
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from links.models import Link
+from links.views import hash_link
+
+
+def create_short_link(full_link):
+    link = Link.objects.create(
+        full_link=full_link,
+        short_link=hash_link(full_link=full_link)
+    )
+    link.save()
+    return link
+
+
+class LinkModelTests(TestCase):
+
+    def test_hash_link(self):
+        tiny_link = hash_link("https://stackoverflow.com/company")
+        self.assertIsNotNone(tiny_link)
+
+    def test_increase_redirects(self):
+        link = create_short_link(
+            "https://stackoverflow.com/company")
+        link.redirects += 1
+        link.save()
+        self.assertIs(link.redirects, 1)
+
+
+class LinkViewTests(TestCase):
+
+    def test_create_link(self):
+        link = create_short_link(
+            "https://stackoverflow.com")
+        url = reverse('links:index')
+        response = self.client.get(url)
+        self.assertContains(response, link.short_link)
+
+    def test_delete_link(self):
+        link = create_short_link(
+            "https://stackoverflow.com")
+        link.delete()
+        url = reverse('links:index')
+        response = self.client.get(url)
+        self.assertNotContains(response, link.full_link)
+
+    def test_nonexistent_link(self):
+        url = reverse('links:index')
+        response = self.client.get(url)
+        full_link = "https://kpfu.ru/"
+        self.assertNotContains(response, full_link)
+
+    def test_view_link(self):
+        link = create_short_link(
+            "https://kpfu.ru/")
+        url = reverse('links:view', args=(link.short_link,))
+        response = self.client.get(url)
+        self.assertRedirects(response,
+                             link.full_link,
+                             status_code=302,
+                             target_status_code=200,
+                             msg_prefix='',
+                             fetch_redirect_response=False)
+
+    def test_test_increase_follow_quantity_after_open_link(self):
+        link = create_short_link(
+            "https://kpfu.com")
+        url = reverse('links:view', args=(link.short_link,))
+        self.client.get(url)
+        updated_link = get_object_or_404(Link, short_link=link.short_link)
+        self.assertIs(updated_link.redirects, link.redirects + 1)
